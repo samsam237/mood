59df5cedef3d0c862a614101fae770aacdede5fb
samsam237 <sbissog@gmail.com>
2025-05-20 16:56:30 +0200

    tab fixed

--- src/components/tab/TabContainer.tsx
+++ src/components/tab/TabContainer.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/react';
-import { IonHeader, IonToolbar, IonTitle } from '@ionic/react';
-import { home, fitness, water, heart, settings, notifications, calendar, trophy, personCircle, thumbsUp, ribbon } from 'ionicons/icons';
-import { IonReactRouter } from '@ionic/react-router';
-import { Redirect, Route } from 'react-router';
-import MainPage from '../../pages/main/MainPage'
+import { home, fitness, notifications, calendar, personCircle, ribbon } from 'ionicons/icons';
 
 import './TabContainer.css';
 import {TabContainerProps} from './TabContainerProps'
@@ -15,40 +11,31 @@ const TabContainer: React.FC<TabContainerProps> = ({ onTabChange }) => {
         <IonRouterOutlet>
             {/* <Route path="/main" component={MainPage} exact /> */}
         </IonRouterOutlet>    
-
         <div className="ion-tab-bar">
             <IonTabBar className='ion-tab-bar-content'>
-
                 <IonTabButton className="custom-tab-button" tab="home" onClick={() => onTabChange('home')}>
                     <div>
                         <IonIcon icon={home} />
                         <IonLabel>Accueil</IonLabel>
                     </div>
                 </IonTabButton>
-
                 <IonTabButton className="custom-tab-button" tab="notifications" onClick={() => onTabChange('notifications')}>
                     <IonIcon icon={notifications} />
                 </IonTabButton>
-
                 <IonTabButton className="custom-tab-button" tab="calendar" onClick={() => onTabChange('calendar')}>
                     <IonIcon icon={calendar} />
                 </IonTabButton>
-
                 <IonTabButton className="custom-tab-button" tab="fitness" onClick={() => onTabChange('fitness')}>
                     <IonIcon icon={fitness} />
                 </IonTabButton>
-
                 <IonTabButton className="custom-tab-button" tab="user" onClick={() => onTabChange('user')}>
                     <IonIcon icon={personCircle} />
                 </IonTabButton>
-
                 <IonTabButton className="custom-tab-button" tab="about" onClick={() => onTabChange('about')}>
                     <IonIcon icon={ribbon} />
                 </IonTabButton>
-
             </IonTabBar>
-        </div>
-      
+        </div>      
     </IonTabs>
   );
 };
