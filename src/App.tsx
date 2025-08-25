fd1091b757bc6030a42dd01755671d0f070c2032
samsam237 <sbissog@gmail.com>
2025-08-25 03:29:48 +0200

    version revisée

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ import '@ionic/react/css/palettes/dark.system.css';
 /* Theme variables */
 import './theme/variables.css';
 import './App.css'
-import PdfViewerContainer from './components/pdfViewer/PdfViewerContainer.old';
+/* import PdfViewerContainer from './components/pdfViewer/PdfViewerContainer.old'; */
 
 setupIonicReact();
 
@@ -100,7 +100,7 @@ const App: React.FC = () => {
             <Route path="/home" component={Home} exact />
             <Redirect from="/" to="/home" exact />
             <Route path="/main" component={MainPage} exact />
-            <Route path="pdfs/view/:file" component={PdfViewerContainer} />
+            {/* <Route path="pdfs/view/:file" component={PdfViewerContainer} /> */}
             <Route path="/welcome" component={Welcome} exact />
             <Route path="/login" component={Login} exact />
           </IonRouterOutlet>        
