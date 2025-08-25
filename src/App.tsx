61686e887beaecdc5d51f0c2bac79f381729ca79
samsam237 <sbissog@gmail.com>
2025-08-25 03:26:29 +0200

    version revisée

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ import '@ionic/react/css/palettes/dark.system.css';
 /* Theme variables */
 import './theme/variables.css';
 import './App.css'
-import PdfViewerContainer from './components/pdfViewer/PdfViewerContainer';
+import PdfViewerContainer from './components/pdfViewer/PdfViewerContainer.old';
 
 setupIonicReact();
 
